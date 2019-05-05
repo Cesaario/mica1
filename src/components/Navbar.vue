96eb5446a67270d765c73787be6e1440a9854c1c
Cesaario <gs.cesario1998@gmail.com>
2019-05-04 22:44:26 -0300

    Adicionada toolbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-toolbar flat app>
+        <v-toolbar-side-icon class='grey--text'></v-toolbar-side-icon>
+        <v-toolbar-title class='grey--text'>
+            <span class='font-weight-light'>MICA</span>
+            <span class='font-weight-bold'>3.0</span>
+        </v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
