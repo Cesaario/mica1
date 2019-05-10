920500f88d5defeab4c657e1c92c7f1c27e08fc6
Cesaario <gs.cesario1998@gmail.com>
2019-05-09 22:27:44 -0300

    Alteração mínima feita no IOExterno

--- src/components/IOExterno.vue
+++ src/components/IOExterno.vue
@@ -1,10 +1,13 @@
 <template>
     <v-bottom-nav :value="true" absolute color="grey" height='50px'>
-        <v-card v-for='(obj,index) in io' :key='obj.nome' :color='corEstado(obj.estado)'>
+        <v-card v-for='obj in ioA' :key='obj.nome' :color='corEstado(obj.estado)'>
             <v-card-text class='ma-0 pa-0 text-xs-center'>{{obj.nome}}</v-card-text>
             <v-card-actions class='ma-0 pa-0' v-if='testMode'>
-                <v-switch class='ma-0 pa-0 justify-center' color='green darken-3' @change='alternar(index)' v-model='obj.estado'></v-switch>
+                <v-switch class='ma-0 pa-0 justify-center' color='green darken-3' v-model='obj.estado'></v-switch>
             </v-card-actions>
+        </v-card>
+        <v-card v-for='obj in ioS' :key='obj.nome' :color='corEstado(obj.estado)'>
+            <v-card-text class='ma-0 pa-0 text-xs-center'>{{obj.nome}}</v-card-text>
         </v-card>
     </v-bottom-nav>
 </template>
@@ -14,19 +17,21 @@
     export default{
         data(){
             return{
-                io: [
-                    {nome:'A0', estado:false},
-                    {nome:'A1', estado:false},
-                    {nome:'A2', estado:false},
-                    {nome:'A3', estado:false},
-                    {nome:'A4', estado:true},
-                    {nome:'A5', estado:false},
-                    {nome:'S6', estado:false},
-                    {nome:'S7', estado:true},
-                    {nome:'S8', estado:false},
-                    {nome:'S9', estado:false},
-                    {nome:'S10', estado:false},
-                    {nome:'S11', estado:false},
+                ioA: [
+                    {nome:'A0', estado:false, tipo: "atuador"},
+                    {nome:'A1', estado:false, tipo: "atuador"},
+                    {nome:'A2', estado:false, tipo: "atuador"},
+                    {nome:'A3', estado:false, tipo: "atuador"},
+                    {nome:'A4', estado:true, tipo: "atuador"},
+                    {nome:'A5', estado:false, tipo: "atuador"}
+                ],
+                ioS:[
+                    {nome:'S6', estado:false, tipo: "sensor"},
+                    {nome:'S7', estado:true, tipo: "sensor"},
+                    {nome:'S8', estado:false, tipo: "sensor"},
+                    {nome:'S9', estado:false, tipo: "sensor"},
+                    {nome:'S10', estado:false, tipo: "sensor"},
+                    {nome:'S11', estado:false, tipo: "sensor"},
                 ],
                 testMode: store.testMode
             }
@@ -37,14 +42,6 @@
             },
             atualizarIO(i, valor){
                 this.io[i].estado = valor;
-            },
-            alternar(i){
-                return;
-                console.log('alternando ', i);
-                console.log('antigo valor: ', this.io[i].estado)
-                if(this.io[i].estado) this.atualizarIO(i, false);
-                else this.atualizarIO(i, true);
-                console.log('novo valor: ', this.io[i].estado)
             }
         }
     }
